213959cfd376cb045ae3834fe43e37f30a10ed37
verial88 <verial88@mail.ru>
2020-10-02 14:07:39 +0300

    доделал задачу парсинга

--- Site-parse/my_site_parsing.py
+++ Site-parse/my_site_parsing.py
@@ -1,35 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-from openpyxl import load_workbook
 import xlrd
-import re
+import os
 
-#Parsing of site https://bashesk.ru/corporate/tariffs/unregulated/limits/
-file_number = 1
+# Парсинг сайта https://bashesk.ru/corporate/tariffs/unregulated/limits/
+if not 'downloads' in os.listdir():
+    os.mkdir('downloads') # создаем директорию для скачивания файлов
+
+# создаем параметры для get-запроса (url + params)
 link = f'https://bashesk.ru/corporate/tariffs/unregulated/limits/'
 params = {
     'filter_name': 'Предельные уровни нерегулируемых цен на электрическую энергию (мощность), поставляемую потребителям (покупателям) ООО "ЭСКБ", (с максимальной мощностью энергопринимающих устройств до 670 кВт)',
     'filter_date_from': '01.07.2019',
     'filter_date_to': '01.07.2020'
 }
-
+# с помощью библиотек requests и BeautifulSoup делаем запрос к сайту и ищем все ссылки на нужные нам файлы (по структуре сайта
+# они находятся в тегах div и классах col-2
 res = requests.get(link, params=params).text
 soup = BeautifulSoup(res, 'lxml')
 all_files = soup.find_all('div', 'col-2')
-
+# нужные нам ссылки имеют тег a и параметр href
 for file in all_files:
     file_link = file.find('a').get('href')
-    download_file = requests.get(f'https://bashesk.ru/{file_link}').content
-
-    with open(f'files/{file_number}.xls', 'wb') as dw_file:
+    file_name = file_link.split('/')
+    download_file = requests.get(f'https://bashesk.ru/{file_link}').content # скачиваем файлы
+# Записываем скачанные файлы в папку files
+    with open(f'downloads/{file_name[-1]}', 'wb') as dw_file:
         dw_file.write(download_file)
 
-    file_number += 1
+# Парсинг файлов
 
-# parsing of files
-book = xlrd.open_workbook('files/1.xls')
-sheet = book.sheet_by_name('1 ц.к. ')
-# print(book.sheet_names())
-pattern = r"^text:\'г) объем фактического пикового потребления гарантирующего поставщика на оптовом рынке, МВт   \'$"
-for row in sheet.get_rows():
-    print(row) #нужно как-то вычленить 'number:'
+for file_pars in os.listdir('downloads'):
+    book = xlrd.open_workbook(f'downloads/{file_pars}')
+    sheet = book.sheet_by_name('1 ц.к. ')
+
+    for row in sheet.get_rows():
+        if 'г) объем фактического пикового потребления гарантирующего поставщика на оптовом рынке, МВт' in row[0].value:
+            print(f'Значение из файла {file_pars}: ', row[15].value)
+            break
--- Trees/my_trees_constructor.py
+++ Trees/my_trees_constructor.py
@@ -1,5 +1,36 @@
 from pyexcel_xlsx import get_data
-data = get_data('example_tree1.xlsx')
-print(data)
+from openpyxl import load_workbook
+
+# метод с использованием библиотеки pyexcel_xlsx
+# data = get_data('example_tree1.xlsx')
+# print(data)
 # import json
 # print(json.dumps(data))
+
+# метод с использованием библиотеки openpyxl
+wb_obj = load_workbook(filename='example_tree1.xlsx', data_only=True)  #data_only режим вывода только информации (значения, а не формулы)
+sheet_obj = wb_obj['Лист1']
+# B1_obj = sheet_obj['B1'].value
+# print(B1_obj)
+# sheet_obj = wb_obj.active
+# print(sheet_obj)
+# cell_obj = sheet_obj.cell(row=1, column=2)
+# print(cell_obj.value)
+# print(sheet_obj.max_column)
+# print(sheet_obj.max_row)
+
+# list_v = []
+# for i in range(1, sheet_obj.max_row + 1):
+#     for j in range(1, sheet_obj.max_column + 1):
+#         list_v.append(sheet_obj.cell(row=i, column=j).value)
+#     print(list_v)
+
+# for row in sheet_obj.values:
+#     for value in row:
+#         print(value)
+
+for row in sheet_obj.iter_rows(min_row=1, max_col=3, values_only=True):
+ if 'класс22' in row:
+  print(row[0])
+
+
